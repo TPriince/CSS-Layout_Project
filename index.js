d01883ced2ac9a0007f6f2409a046db34a6f7305
Priince <tobiloba110@gmail.com>
2022-09-08 12:06:39 +0100

    Add index.js file

---
+++ index.js
@@ -0,0 +1,7 @@
+// let inputElement = document.getElementById("input1");
+// const paragraph = document.getElementById("para");
+
+
+// inputElement.addEventListener('input', function() {
+//     paragraph.textContent = this.value;
+// })
